e24f6368e332cf2461c5ba776158915d04511ac4
Astramast <azerqsdfazerqsdg@gmail.com>
2024-12-09 20:16:08 +0100

    finished cpu producer

--- producers/cpu-temperature-linux.py
+++ producers/cpu-temperature-linux.py
@@ -1,13 +1,12 @@
 from kafka import KafkaProducer
 
-broker = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: int(v).to_bytes(4, byteorder='big'))
 
 i = 10
 while i:
 	with open("/sys/class/thermal/thermal_zone0/temp", "r", encoding="utf-8") as f:
 		temperature = int(f.read())
 		print(f"{temperature/1000}°C")
-		message = { "temperature": temperature }
-		broker.send('advanceddb', value=temperature)
+		producer.send('cpu', value=temperature)
 	i -= 1
 
