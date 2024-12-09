4675b2e4c072ea29fe22580cddb49107522e3d61
Astramast <azerqsdfazerqsdg@gmail.com>
2024-12-09 21:53:08 +0100

    start for cpu flink

---
+++ consumers/flink/cpu/src/main/java/com/flink/IntegerDeserializationScheme.java
@@ -0,0 +1,25 @@
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class IntegerDeserializationSchema implements DeserializationSchema<Integer> {
+
+    @Override
+    public Integer deserialize(byte[] message) throws IOException {
+        // Convert the 4-byte big-endian data into an Integer
+        return ByteBuffer.wrap(message).getInt();
+    }
+
+    @Override
+    public boolean isEndOfStream(Integer nextElement) {
+        return false;
+    }
+
+    @Override
+    public TypeInformation<Integer> getProducedType() {
+        return TypeInformation.of(Integer.class);
+    }
+}
+
