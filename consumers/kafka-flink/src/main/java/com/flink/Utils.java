55baaf2d1c824c67dd9921b989f21b7d3f14cf80
Bluemana <1xujason@gmail.com>
2024-12-10 14:47:21 +0100

    adding a csv repair to repair csv for utf-8 and adding banned user

--- consumers/kafka-flink/src/main/java/com/flink/Utils.java
+++ consumers/kafka-flink/src/main/java/com/flink/Utils.java
@@ -1,16 +1,23 @@
 package com.flink;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Utils {
+    static HashSet<String> bannedUser = new HashSet<String>();
     public static void processMessage(String value, String[] bannedWords) throws IOException {
         Map<String, String> valueMap = new ObjectMapper().readValue(value, new TypeReference<Map<String, String>>() {
         });
-
+        if (Utils.checkUserIsBan(valueMap.get("user"))){
+            System.out.println("\u001B[31m" +
+                    valueMap.get("date") + " || " + valueMap.get("user") + ": User Banned"
+                    + "\u001B[0m");
+            return;
+        }
         if (Utils.checkWordBan(valueMap.get("text"), bannedWords)) {
             System.out.println("\u001B[31m" +
                     valueMap.get("date") + " || " + valueMap.get("user") + ": Message contains banned word"
@@ -27,5 +34,8 @@ public class Utils {
             }
         }
         return false;
+    }
+    public static boolean checkUserIsBan(String user){
+        return bannedUser.contains(user);
     }
 }
---
+++ producers/csv_repaired.py
@@ -0,0 +1,7 @@
+lines = []
+with open('../chat.csv', 'r', encoding='utf-8', errors='replace') as f:
+    lines = f.readlines()
+
+# Write the cleaned lines to a new file
+with open('../cleaned_chat.csv', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
