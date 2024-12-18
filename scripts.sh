b4914cbd72b80e9677755cfba9501d2979ad6de7
Astramast <azerqsdfazerqsdg@gmail.com>
2024-12-18 19:42:02 +0100

    Wed 18 Dec 19:42:02 CET 2024

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 /opt/kafka/bin/kafka-topics.sh --bootstrap-server localhost:9092 --topic chat --delete
 /opt/kafka/bin/kafka-topics.sh --create --topic chat --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
