8c45ab258e511e84045bf0b02470fdc81d8ba094
Astramast <azerqsdfazerqsdg@gmail.com>
2024-12-18 20:31:28 +0100

    Wed 18 Dec 20:31:28 CET 2024

---
+++ installationArch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+separator() {
+	echo "$(printf '%.0s-' {1..40})"
+}
+
+separator
+echo "Project Installer"
+separator
+
+separator
+echo "Updating..."
+separator
+sudo pacman -Syu
+
+separator
+echo "Installing packages..."
+separator
+sudo pacman -S yay
+yay openjdk11 python3-venv
+
+separator
+echo "Installing Kafka..."
+separator
+yay kafka
+
+separator
+echo "Installing Flink..."
+separator
+yay apache-flink
+
+separator
+echo "Initalizing python venv..."
+separator
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+separator
+echo "Done !"
+separator
+
---
+++ installationUbuntu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+separator() {
+	echo "$(printf '%.0s-' {1..40})"
+}
+
+separator
+echo "Project Installer"
+separator
+
+separator
+echo "Updating..."
+separator
+sudo apt update
+
+separator
+echo "Installing packages..."
+separator
+sudo apt install openjdk-11-jdk python3-venv
+
+separator
+echo "Installing Kafka..."
+separator
+wget https://downloads.apache.org/kafka/3.9.0/kafka_2.12-3.9.0.tgz
+sudo mv kafka_2.12-3.9.0.tgz /opt/kafka
+rm kafka_2.12-3.9.0.tgz
+sudo mkdir -p /tmp/zookeeper/version-2
+sudo chown -R $USER:$USER /tmp/zookeeper
+sudo mkdir -p /tmp/kafka-logs
+sudo chown -R $USER:$USER /tmp/kafka-logs
+
+separator
+echo "Installing Flink..."
+separator
+wget https://archive.apache.org/dist/flink/flink-1.14.4/flink-1.14.4-bin-scala_2.12.tgz
+sudo mv flink-1.14.4-bin-scala_2.12.tgz /opt/flink
+rm flink-1.14.4-bin-scala_2.12.tgz
+
+separator
+echo "Initalizing python venv..."
+separator
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+separator
+echo "Done !"
+separator
+
