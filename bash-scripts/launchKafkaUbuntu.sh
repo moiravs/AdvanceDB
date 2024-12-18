609eb1a04457bd4a10bc3d4f62e4a8b14d7da5b3
Astramast <azerqsdfazerqsdg@gmail.com>
2024-12-18 21:28:18 +0100

    Wed 18 Dec 21:28:18 CET 2024

--- bash-scripts/launchKafkaUbuntu.sh
+++ bash-scripts/launchKafkaUbuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties 2>&1 zookeper-server.log &
+/opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties > zookeper-server.log  2>&1 &
 
-/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties 2>&1 kafka-server.log &
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties > kafka-server.log 2>&1  &
 
 /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic chat
 
